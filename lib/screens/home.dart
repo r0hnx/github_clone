cef92aee14e3c5a7e4c4bb40c1c03323073c3ccb
EANimesha <nimeshadilini999@gmail.com>
2020-10-11 00:11:53 +0530

    added app bar to home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,10 +8,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _currentstate=0;
+  int _currentstate = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: CircleAvatar(
+          backgroundImage: NetworkImage("https://i.redd.it/6onq25y0sh311.jpg"),
+          backgroundColor: Colors.transparent,
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add_circle_outline, color: Colors.blue),
+            onPressed: () => print("+"),
+          )
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.white,
         fixedColor: Colors.blue,
@@ -19,20 +33,18 @@ class _HomePageState extends State<HomePage> {
         items: [
           BottomNavigationBarItem(
             icon: new Icon(Icons.home),
-            label:'Home',
+            title: Text('Home'),
           ),
           BottomNavigationBarItem(
             icon: new Icon(Icons.notifications),
-            label:'Notifications',
+            title: Text('Notifications'),
           ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label:'Explore'
-          )
+              icon: Icon(Icons.search), title: Text('Explore'))
         ],
-        onTap: (index){
+        onTap: (index) {
           setState(() {
-            _currentstate=index;
+            _currentstate = index;
           });
         },
       ),
