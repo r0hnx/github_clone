a22dcab595e2d93d3da6378a52aa5c6dac0dd790
EANimesha <nimeshadilini999@gmail.com>
2020-10-11 01:26:30 +0530

    added my work widget to home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,6 +11,7 @@ class _HomePageState extends State<HomePage> {
   int _currentstate = 0;
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         title: CircleAvatar(
@@ -25,6 +26,37 @@ class _HomePageState extends State<HomePage> {
             onPressed: () => print("+"),
           )
         ],
+      ),
+      body: Container(
+        padding: EdgeInsets.all(height * 0.02),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Text(
+            "Home",
+            style:
+                TextStyle(fontSize: height * 0.05, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: height * 0.04,
+          ),
+          Text(
+            "My Work",
+            style: TextStyle(fontSize: height * 0.03),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: Column(
+              children: [
+                Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(15.0),
+                      color: Colors.white,
+                    ),
+                    child: _myWork()),
+              ],
+            ),
+          ),
+        ]),
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.white,
@@ -50,4 +82,63 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+
+//my Work Widget 
+  Widget _myWork() {
+    double width = MediaQuery.of(context).size.width;
+    return (Column(
+      children: [
+        _myWorkItem("Issues", Icon(
+                Icons.error_outline,
+                color: Colors.white,
+              ), Colors.green),
+        Divider(
+          indent: width * 0.15,
+        ),
+        _myWorkItem("Pull Requests", Icon(
+                Icons.call_merge,
+                color: Colors.white,
+              ), Colors.blue),
+        Divider(
+          indent: width * 0.15,
+        ),
+        _myWorkItem("Repositories", Icon(
+                Icons.filter_none,
+                color: Colors.white,
+              ), Colors.purple),
+        Divider(
+          indent: width * 0.15,
+        ),
+        _myWorkItem("Organizations", Icon(
+                Icons.people_outline,
+                color: Colors.white,
+              ), Colors.orange),
+      ],
+    ));
+  }
+
+//my work item component
+  Widget _myWorkItem(String title,Icon icon,Color color){
+   return(
+      ListTile(
+          visualDensity: VisualDensity(horizontal: 0, vertical: -4),
+          leading: Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(4.0),
+                color: color,
+              ),
+              padding: EdgeInsets.all(4.0),
+              child: icon),
+          title: Text(
+            title,
+            style: TextStyle(fontSize: 18.0),
+          ),
+          trailing: Icon(
+            Icons.arrow_forward_ios,
+            size: 20.0,
+          ),
+        )
+   );
+ }
 }
